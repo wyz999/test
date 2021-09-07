69597efc0136a10f8a83ef1cf09acfd78d21a752
wyz999 <50292774+wyz999@users.noreply.github.com>
2021-09-07 09:56:28 +0800

    Revert "clear"

--- mian.go
+++ mian.go
@@ -1,23 +1,10 @@
 package main
 
-
 import (
 	"fmt"
 )
 
-func gotest(n int) int{
-	a,b:=0,1
-	for n>0{
-		a,b=b,a+b
-		fmt.Println(a)
-		n--
-	}
-	return a
-}
 
 func main() {
 	fmt.Println("666666")
-	fmt.Println("git test")
-	fmt.Println(gotest(6))
-	
 }
